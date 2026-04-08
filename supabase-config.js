4078512f5a8a181c689c5ca47af49dfdc78fb90c
hubj66 <49588932+hubj66@users.noreply.github.com>
2026-04-08 13:52:24 +0200

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://qfjzhaejqtupofxwmapo.supabase.co";
+const SUPABASE_KEY = "sb_publishable_qox5SpcAY3mEBU4HNbMxcQ_u-TrEmT_";
+
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
